255051406691c3126f5c4786dc3fde5bbd375e89
Aksel Stankiewicz <aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eu>
2023-11-21 19:26:57 +0100

    Replacing spaces with %20

--- src/main/java/org/example/services/WeatherService.java
+++ src/main/java/org/example/services/WeatherService.java
@@ -4,12 +4,17 @@ import org.example.api.HttpClientService;
 import org.example.api.open_weather.CityOwResponse;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WeatherService {
 	
 	public CityOwResponse getWeatherFromOpenWeather(Scanner sc) {
 		System.out.println("podaj nazwę miasta");
 		String city=sc.nextLine().trim();
+		Pattern pattern = Pattern.compile(" ");
+		Matcher matcher = pattern.matcher(city);
+		city = matcher.replaceAll("%20");
 		String openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=716238e6166ce6e1315daf3232959cd3&q="+city+"&units=metric";
 		
 		var httpClientService = new HttpClientService<CityOwResponse>();
