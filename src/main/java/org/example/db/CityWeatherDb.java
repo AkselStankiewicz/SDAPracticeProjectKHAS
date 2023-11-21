9aa08a2de17ab9968cb3bcc1c99dbc421f0f8777
Odyn666 <lambertmorhen@wp.pl>
2023-11-21 19:19:47 +0100

    save to database added

--- src/main/java/org/example/db/CityWeatherDb.java
+++ src/main/java/org/example/db/CityWeatherDb.java
@@ -8,6 +8,13 @@ public class CityWeatherDb {
     private static final Map<String, CityDataEntity> dataBase = new HashMap<>();
 
 
+    boolean save(String key, CityDataEntity city) {
+        if (dataBase.containsKey(key))
+            return false;
+        dataBase.put(key, city);
+        return true;
+    }
+
     boolean remove(String key, CityDataEntity city) {
         if (dataBase.containsKey(key) || dataBase.containsValue(city)) {
             dataBase.remove(key, city);
