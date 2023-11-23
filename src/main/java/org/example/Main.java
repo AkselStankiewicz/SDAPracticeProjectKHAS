584cdc71fea95c01738adbd98fe9bebf3075dd20
Aksel Stankiewicz <aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eu>
2023-11-23 20:01:01 +0100

    deleted hello world from main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
         showWelcomeMenu();
         CityService cityService = new CityService();
         CityWeatherDb cityWeatherDb = new CityWeatherDb();
